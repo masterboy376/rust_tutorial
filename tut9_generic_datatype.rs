ae1b70559e4bc7b36c74ea75d526787b2d514d3f
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-27 21:14:59 +0530

    Create tut9_generic_datatype.rs

---
+++ tut9_generic_datatype.rs
@@ -0,0 +1,59 @@
+fn main() {
+
+    //--------------generic datatype--------------
+    // function
+    // fn largest<T>(list: &[T]) -> &T { // error as T must be comparable type
+    fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+        let mut largest = &list[0];
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+        largest
+    }
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+    // struct
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    struct Point<T, U> {
+        x: T,
+        y: U,
+    }
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    //  enum
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+    // method
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+    impl<T> Point<T> {
+        fn x(&self) -> &T {
+            &self.x
+        }
+    }
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+}
+
+
